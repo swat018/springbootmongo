0bc18f2cfe1edcf2bf2d6ee54a262461faa582e5
jinwoo park <swat018@gmail.com>
2022-01-01 23:54:53 +0900

    feat: 스프링 데이터 10부: MongoDB

---
+++ src/main/java/com/swat018/springbootmongo/Account/AccountRepository.java
@@ -0,0 +1,9 @@
+package com.swat018.springbootmongo.Account;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+import java.util.Optional;
+
+public interface AccountRepository extends MongoRepository<Account, String> {
+    Optional<Account> findByEmail(String email);
+}
--- src/main/java/com/swat018/springbootmongo/SpringbootmongoApplication.java
+++ src/main/java/com/swat018/springbootmongo/SpringbootmongoApplication.java
@@ -1,13 +1,38 @@
 package com.swat018.springbootmongo;
 
+import com.swat018.springbootmongo.Account.Account;
+import com.swat018.springbootmongo.Account.AccountRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.mongodb.core.MongoTemplate;
 
 @SpringBootApplication
 public class SpringbootmongoApplication {
 
+//    @Autowired
+//    MongoTemplate mongoTemplate;
+
+    @Autowired
+    AccountRepository accountRepository;
+
     public static void main(String[] args) {
         SpringApplication.run(SpringbootmongoApplication.class, args);
+    }
+
+    @Bean
+    public ApplicationRunner applicationRunner() {
+        return args -> {
+            Account account = new Account();
+            account.setEmail("swat018@icloud.com");
+            account.setUsername("swat018");
+//            mongoTemplate.insert(account);
+            accountRepository.insert(account);
+
+            System.out.println("finished");
+        };
     }
 
 }
